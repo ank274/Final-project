8f57de4f714d3e62877f8c3316e395107d0e5d3d
au643 <51296607+au643@users.noreply.github.com>
2019-06-30 15:46:57 -0400

    Filters CSV file and appends items to subscription list

--- Project.py
+++ Project.py
@@ -1,1 +1,18 @@
+import csv
+subscriptions = []
+while True:
+    subscription = input("Please provide your monthly subscriptions or DONE if finished: ")
+    if subscription == "DONE":
+        break
+    else: 
+        subscriptions.append(subscription)
 
+
+csv_file_path = "bank_accounts.csv" # a relative filepath
+
+with open(csv_file_path, 'rt') as f:
+    reader = csv.reader(f, delimiter=',')
+    for row in reader:
+        for subscription in subscriptions:
+            if subscription in row[2]:
+                print(row[2], row[3])
